e60405edd04425aa1de38250ffa9caec2214445e
minwood <matt.inwood@gmail.com>
2018-10-05 06:16:47 -0500

    Cleaned up some prior code, added map rendering for horizontal stacking hex maps.

--- hexmap.py
+++ hexmap.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon
 import numpy as np
+
 
 class Tile:
     def __init__(self, terrain=None, occupant=None):
@@ -15,14 +18,14 @@ class Tile:
             'vegetation': 0
             }
 
-    # def neighbors(self, map_object):
+        self.color = 'b'
 
 
 class Map:
     def __init__(self, width=None, height=None):
         self.width = width
         self.height = height
-
+        self.map = []
 
         if (width is not None) and (height is not None):
             self.init_map()
@@ -36,7 +39,7 @@ class Map:
             'SW': [1, -1],
         }
 
-    def clear_hex_coordinates(self):
+    def clear_map(self):
         for y in range(len(self.map)):
             for x in range(len(self.map[y])):
                 if (y % 2 == 0) and (x % 2 == 0):
@@ -47,22 +50,8 @@ class Map:
                     self.map[y][x] = Tile()
 
     def init_map(self):
-        self.map = np.zeros((self.height, self.width)).tolist()
-        self.clear_hex_coordinates()
-
-    # Generates a horizontal stacked hex map
-    def generate(self, shape='square'):
-        if shape == 'square':
-            for y in range(self.height):
-                for x in range(self.width):
-                    if (y % 2 == 0):
-                        self.map.append(Tile(coordinates=(x, y/2)))
-                    else:
-                        self.map.append(Tile(coordinates=(x+0.5, y/2)))
-        elif shape == 'hex':
-            pass
-        else:
-            raise NameError('No shape with name "{0}"'.format(shape))
+        self.map = np.zeros((self.height, (self.width*2)-1)).tolist()
+        self.clear_map()
 
     def range_one(self, x, y):
         neighbors = []
@@ -71,3 +60,26 @@ class Map:
 
         for neighbor in neighbors:
             print(neighbor)
+
+    def render(self):
+        fig, ax = plt.subplots(figsize=(5,5), dpi=300)
+        ax.set_aspect('equal')
+        # TODO: Figure out proper math for x multiplier
+        for x in range((self.width*2)-1):
+            for y in range(self.height):
+                if self.map[y][x] is not None:
+                    hex = RegularPolygon((x*0.58, y),
+                                         numVertices=6,
+                                         radius=2/3,
+                                         orientation=0,
+                                         facecolor=self.map[y][x].color,
+                                         alpha=0.2,
+                                         edgecolor='k')
+                    ax.add_patch(hex)
+        plt.xlim(-3/4, self.width*1.13)
+        plt.ylim(-3/4, self.height)
+        plt.show()
+
+
+m = Map(width=13, height=9)
+m.render()
